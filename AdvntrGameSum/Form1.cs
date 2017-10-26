62381ffdf42f09e7061d871749ec73b3fcda8150
nichpuet <napuetz@gmail.com>
2017-10-26 12:25:47 -0400

    Add project files.

---
+++ AdvntrGameSum/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvntrGameSum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.B)
+            {
+
+            }
+            if (e.KeyCode == Keys.R)
+            {
+
+            }
+        }
+    }
+}
